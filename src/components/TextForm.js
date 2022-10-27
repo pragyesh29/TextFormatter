62df0e4a061ae73e794bbcced7c590e7fc67e6f8
Pragyesh <pragyeshtiwari29@gmail.com>
2022-10-28 00:51:10 +0530

    Adding TextForm Component and using useState Hook to understand state

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export default function TextForm(props) {
+
+    const handleUpperCase = () => {
+        setText(text.toUpperCase())
+    }
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState('Enter text...');
     return (
         <div>
             <h1>{props.heading}</h1>
             <div className="my-3">
-                <textarea id="my-box" rows="5" className="form-control"></textarea>
+                <textarea id="my-box" rows="5" className="form-control" onChange={handleOnChange} value={text}></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Uppercase</button>
+            <button className="btn btn-primary" onClick={handleUpperCase}>Convert to Uppercase</button>
         </div>
     )
 }
